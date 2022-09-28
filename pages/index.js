18954435cedb6e8233b347ea6a7ead5d39faa361
Belati <belatijagadbintangsyuhada@gmail.com>
2022-09-28 20:52:06 +0700

    Another try

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Button from './components/Button';
-// import Display from './components/Display';
 import Footer from './components/Footer';
 import Search from './components/Search';
 
